7710af1f34ee7bd74c4bcef6e23f45c61210cd31
Kunal Sonawane <73280919+kunalsonawane3@users.noreply.github.com>
2021-06-20 18:45:56 +0000

    main

--- insertion_sort-challenge/implement_insert.py
+++ insertion_sort-challenge/implement_insert.py
@@ -5,13 +5,14 @@ def insert(arr, right_index, value):
     right_index = right_index - 1
   arr[right_index+1] = value
 
-array = [3, 5, 7, 11, 13, 2, 9, 6]
+if __name__ == "__main__":
+  array = [3, 5, 7, 11, 13, 2, 9, 6]
 
-insert(array, 4, 2)
-print(array)
+  insert(array, 4, 2)
+  print(array)
 
-insert(array, 5, 9)
-print(array)
+  insert(array, 5, 9)
+  print(array)
 
-insert(array, 6, 6)
-print(array)
+  insert(array, 6, 6)
+  print(array)
